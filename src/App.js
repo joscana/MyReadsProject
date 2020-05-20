eae06f92b214f1272eaf9b08866eda9ee48210c8
Joscana-Garcia <59126118+joscana@users.noreply.github.com>
2020-05-20 12:11:46 -0400

    Console logging searchbar query

--- src/App.js
+++ src/App.js
@@ -80,30 +80,22 @@ class BooksApp extends React.Component {
             <div className="search-books-bar">
               <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
               <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                {JSON.stringify(this.state.query)}
+                  {/* NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                {/* const { query } = this.state.query
-                const { books } = this.props
-
-                const showingBooks = query === '' 
-                ? books
-                : books.filter((b) => (
-                  b.title.toLowerCase().includes(query.toLowerCase())
-                )) */}
+                  you don't find a specific author or title. Every search is limited by search terms. */}
+                <form>
                 <input
                 className="search-books"
                 type="text" 
                 placeholder="Search by title or author"
-                // value={query}
+                value={this.state.query}
                 onChange={(event) => this.updateQuery(event.target.value)}
                 />
-
+                </form>
               </div>
             </div>
             <div className="search-books-results">
